218e6c197cacc3dcdf86c43dad216305bb7ab846
zjjaxx <2495893564@qq.com>
2021-12-19 21:59:10 +0800

    feat: 开发环境一键启动

---
+++ scripts/dev.js
@@ -0,0 +1,20 @@
+const { spawn }=require("child_process") 
+const execCmd = (params) => {
+    const cmd = spawn(process.platform === 'win32' ? 'yarn.cmd' :'yarn', params);
+    cmd.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    cmd.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+    cmd.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+(async function main() {
+    execCmd(["run", "build:watch"])
+    execCmd(["run", "build:theme:watch"])
+    execCmd(["run", "dev"])
+})()
+
