122ee2060dcade87d50f616be751af7e5ad0bf5b
zjjaxx <2495893564@qq.com>
2022-01-09 22:44:33 +0800

    feat: button

--- packages/sports_boy/src/components/z-button/z-button.vue
+++ packages/sports_boy/src/components/z-button/z-button.vue
@@ -1,24 +1,35 @@
 <template>
-  <button :class="['z-button', 'z-' + props.size + '-button']">
-    <slot></slot>
+  <button
+    :disabled="disable"
+    :class="[
+      'z-button',
+      props.size && 'z-' + props.size + '-button',
+      props.type && 'z-type-' + props.type,
+      round && 'z-round',
+      disable && 'z-disable',
+    ]"
+  >
+    <slot name="icon">
+      <ZIcon v-if="icon" class="z-icon-right" :name="icon"></ZIcon>
+    </slot>
+    <slot>按钮</slot>
   </button>
 </template>
 <script setup lang="ts">
+import ZIcon from "../z-icon/z-icon.vue";
 interface Props {
   // 尺寸
   size: "medium" | "small" | "mini";
   // 类型
-  type: "primary" | "success" | "warning" | "danger" | "info" | "text";
-  // 是否朴素按钮
-  plain: boolean;
+  type: "primary" | "success" | "warning" | "danger" | "info";
   // 是否圆角按钮
   round: boolean;
-  // 是否加载中状态
-  loading: boolean;
   //是否禁用状态
   disable: boolean;
   //图标类名
   icon: string;
+  // 是否加载中状态
+  loading: boolean;
   // 是否默认聚焦
   autofocus: boolean;
   // 原生
--- packages/website/src/App.vue
+++ packages/website/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import {ref} from "vue"
-const size=ref("default")
-const sizeChange=(_size:string)=>{
-  size.value=_size
-}
+import { ref } from "vue";
+const size = ref("default");
+const sizeChange = (_size: string) => {
+  size.value = _size;
+};
+const type = ref();
+const typeChange = (_type: string) => {
+  type.value = _type;
+};
 </script>
 
 <template>
@@ -29,19 +33,216 @@ const sizeChange=(_size:string)=>{
     </ZCol>
   </ZRow>
   <ZIcon></ZIcon>
-  <ZRow>
+  <ZRow class="row-1">
     <ZCol :span="6">
-       <ZButton :size="size" style="margin-left: 10px;" @click="">default</ZButton>
-    </ZCol> 
+      <ZButton :size="size" style="margin-left: 10px" @click=""
+        >默认按钮</ZButton
+      >
+    </ZCol>
     <ZCol :span="6">
-       <ZButton size="medium" style="margin-left: 10px;" @click="sizeChange('medium')">medium</ZButton>
-    </ZCol> 
-     <ZCol :span="6">
-       <ZButton size="small" style="margin-left: 10px;" @click="sizeChange('small')">small</ZButton>
-    </ZCol> 
-     <ZCol :span="6">
-       <ZButton size="mini" style="margin-left: 10px;" @click="sizeChange('mini')">mini</ZButton>
-    </ZCol> 
+      <ZButton
+        size="medium"
+        style="margin-left: 10px"
+        @click="sizeChange('medium')"
+        >中等按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="6">
+      <ZButton
+        size="small"
+        style="margin-left: 10px"
+        @click="sizeChange('small')"
+        >小型按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="6">
+      <ZButton size="mini" style="margin-left: 10px" @click="sizeChange('mini')"
+        >超小按钮</ZButton
+      >
+    </ZCol>
+  </ZRow>
+  <ZRow class="row-1">
+    <ZCol :span="4">
+      <ZButton :type="type" style="margin-left: 10px">default</ZButton>
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        type="primary"
+        style="margin-left: 10px"
+        @click="typeChange('primary')"
+        >主要按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton type="info" style="margin-left: 10px" @click="typeChange('info')"
+        >通知按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        type="warning"
+        style="margin-left: 10px"
+        @click="typeChange('warning')"
+        >警告按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        type="danger"
+        style="margin-left: 10px"
+        @click="typeChange('danger')"
+        >危险按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        type="success"
+        style="margin-left: 10px"
+        @click="typeChange('success')"
+        >成功按钮</ZButton
+      >
+    </ZCol>
+  </ZRow>
+  <ZRow class="row-1">
+    <ZCol :span="4">
+      <ZButton
+        round
+        size="medium"
+        style="margin-left: 10px"
+        @click="sizeChange('medium')"
+        >中等按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        size="small"
+        round
+        style="margin-left: 10px"
+        @click="sizeChange('small')"
+        >小型按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        round
+        size="mini"
+        style="margin-left: 10px"
+        @click="sizeChange('mini')"
+        >超小按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        round
+        type="primary"
+        size="medium"
+        style="margin-left: 10px"
+        @click="sizeChange('medium')"
+        >中等按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        size="small"
+        type="primary"
+        round
+        style="margin-left: 10px"
+        @click="sizeChange('small')"
+        >小型按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        round
+        size="mini"
+        type="primary"
+        style="margin-left: 10px"
+        @click="sizeChange('mini')"
+        >超小按钮</ZButton
+      >
+    </ZCol>
+  </ZRow>
+  <ZRow class="row-1">
+    <ZCol :span="4">
+      <ZButton
+        disable
+        type="primary"
+        style="margin-left: 10px"
+        @click="typeChange('primary')"
+        >主要按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        disable
+        type="info"
+        style="margin-left: 10px"
+        @click="typeChange('info')"
+        >通知按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        disable
+        type="warning"
+        style="margin-left: 10px"
+        @click="typeChange('warning')"
+        >警告按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        disable
+        type="danger"
+        style="margin-left: 10px"
+        @click="typeChange('danger')"
+        >危险按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="4">
+      <ZButton
+        disable
+        type="success"
+        style="margin-left: 10px"
+        @click="typeChange('success')"
+        >成功按钮</ZButton
+      >
+    </ZCol>
+  </ZRow>
+  <ZRow class="row-1">
+    <ZCol :span="6">
+      <ZButton
+        icon="share"
+        round
+        type="primary"
+        size="medium"
+        style="margin-left: 10px"
+        @click="sizeChange('medium')"
+        >中等按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="6">
+      <ZButton
+        icon="share"
+        size="small"
+        type="primary"
+        round
+        style="margin-left: 10px"
+        @click="sizeChange('small')"
+        >小型按钮</ZButton
+      >
+    </ZCol>
+    <ZCol :span="6">
+      <ZButton
+        icon="share"
+        round
+        size="mini"
+        type="primary"
+        style="margin-left: 10px"
+        @click="sizeChange('mini')"
+        >超小按钮</ZButton
+      >
+    </ZCol>
   </ZRow>
 </template>
 
@@ -49,6 +250,9 @@ const sizeChange=(_size:string)=>{
 .row {
   margin-top: 10px;
   background-color: #eee;
+}
+.row-1 {
+  margin-top: 10px;
 }
 .col-content {
   background: #777;
